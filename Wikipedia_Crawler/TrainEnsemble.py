6e6cfc9355fd6c6c07d147f99cb443ceac9831f2
nbodapati <bodapatigus16076@gmail.com>
2017-12-30 15:09:44 -0600

    Add files via upload

--- Wikipedia_Crawler/TrainEnsemble.py
+++ Wikipedia_Crawler/TrainEnsemble.py
@@ -4,6 +4,8 @@ import numpy as np
 from collections import defaultdict,OrderedDict
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier as DTC
+import pickle
+
 df=pd.read_csv('./Optimal_Phrases.csv')
 
 pos_df=df[df['quality']==1]
@@ -66,9 +68,10 @@ def train_ensemble(X_train,y_train):
     n_trees=5
     #should be as diverse and good in accuracy as possible.
     #list of decision trees - each trained to unpruned depth.
-    dtrees=[DTC(criterion='entropy') for _ in range(n_trees)]
+    dtrees=[DTC(criterion='entropy',max_features='log2') for _ in range(n_trees)]
     trained_trees=[]
     for dt in dtrees:
+        print(dt)
         dt.fit(X_train,y_train) 
         trained_trees.append(dt)
 
@@ -82,10 +85,33 @@ def predict_tree(tree,X_test,Y_test):
     return pred
 
 def combined_accuracy(preds,y_test):
+    global vocab
     preds=np.array(preds)
+    print("Preds shape: ",preds.shape)
     #shape - TxN
     fpred=np.mean(preds,axis=0,keepdims=True)
-    return np.mean(fred==y_test)
+    print(fpred.shape)
+    preds_df=pd.DataFrame(preds)
+    preds_df['y_test']=y_test
+    #preds_df['fpred']=fpred
+
+    return (preds_df,0)
+
+def map_vector2sentence(vector,new_vocab):
+    #vector - sparse matrix with 1s at locations of words
+    #new_vocab - list of words that are used in building the features.
+    new_vocab=np.array(new_vocab)
+    vector=vector.tolist() 
+    selected_words=new_vocab[vector]
+    sentence=' '.join(selected_words)
+    print(sentence)
+
+def get_testsentences(x_test,new_vocab):
+    sentences=[]
+    for vector in range(x_test.shape[0]):
+        vec=x_test[vector,:]
+        sentences.append(map_vector2sentence(vec,new_vocab))
+    return sentences
 
 if __name__ =='__main__':
    build_vocab()
@@ -97,7 +123,7 @@ if __name__ =='__main__':
    print_list(np.sum(features,axis=1))
 
    X_train, X_test, y_train, y_test = train_test_split(\
-      features, labels, test_size=0.40, random_state=42)
+      features, labels, test_size=0.45, random_state=42)
    print(X_train.shape,X_test.shape,y_train.shape,y_test.shape)
    
    trained_trees=train_ensemble(X_train,y_train)
@@ -105,5 +131,11 @@ if __name__ =='__main__':
    for tree in trained_trees:
        preds.append(predict_tree(tree,X_test,y_test))   
    
-   cacc=combined_accuracy(preds,y_test)  
+   preds_df,cacc=combined_accuracy(preds,y_test)  
+   test_df=pd.DataFrame(get_testsentences(X_test,new_vocab))
+   #preds_df=pd.concat([preds_df,test_df])
+   test_df.to_csv('./TestSet.csv')
+   preds_df.to_csv('./Predictions_Ensemble.csv')
+   pickle.dump(vocab,open('vocab.pkl','wb'))
+   
    print("Combined accuracy: ",cacc)
