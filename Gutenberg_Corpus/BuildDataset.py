0b9d909d2b3d094d5cd24e60921f322e12e03c8b
nbodapati <bodapatigus16076@gmail.com>
2018-01-02 22:54:05 -0600

    Add files via upload

--- Gutenberg_Corpus/BuildDataset.py
+++ Gutenberg_Corpus/BuildDataset.py
@@ -9,9 +9,9 @@ KB=pickle.load(open('KnowledgeBase.pkl','rb'))
 unigram_vocab=pickle.load(open('unigram_vocab.pkl','rb'))
 bigram_vocab=pickle.load(open('bigram_vocab.pkl','rb'))
 trigram_vocab=pickle.load(open('trigram_vocab.pkl','rb'))
-fourgram_vocab=pickle.load(open('fourgram_vocab.pkl','rb'))
-fivegram_vocab=pickle.load(open('fivegram_vocab.pkl','rb'))
-sixgram_vocab=pickle.load(open('sixgram_vocab.pkl','rb'))
+#fourgram_vocab=pickle.load(open('fourgram_vocab.pkl','rb'))
+#fivegram_vocab=pickle.load(open('fivegram_vocab.pkl','rb'))
+#sixgram_vocab=pickle.load(open('sixgram_vocab.pkl','rb'))
 #bigram_pos=pickle.load(open('bigram_pos.pkl','rb'))
 #trigram_pos=pickle.load(open('trigram_pos.pkl','rb'))
 
@@ -67,35 +67,6 @@ def rectify_frequencies_Pos(d,d2):
         new_d[words]=freq*d2[tags]/num_d2  
     return new_d
 
-def optimal_phrases():
-    global bigram_vocab,trigram_vocab
-    optimal=[]
-    quality=[]
-    num_items=len(trigram_vocab.keys())
-    for key,v in trigram_vocab.items():
-        print("Original phrase:",key,"To go...",num_items)
-        score1=v
-        words=key.split() 
-        score2=bigram_vocab[' '.join(words[:-1])]  
-        score3=bigram_vocab[' '.join(words[1:])]  
-        scores=[score1,score2,score3]
-        phrases=[key,' '.join(words[:-1]),' '.join(words[1:])]
-        best_score=max(scores)
-        idx=scores.index(best_score)
-        optimal.append(phrases[idx])
-        try:
-           if(best_score<1):
-              quality.append(0)
-           else:
-              quality.append(1)
-           #q=eval(input('Prompt quality(0/1): %s'%(phrases[idx]))) 
-           #quality.append(int(q))
-           num_items-=1
-        except:
-           break 
-
-    optimal_df=pd.DataFrame(dict(optimal_phrases=optimal,quality=quality)) 
-    optimal_df.to_csv('./Optimal_Phrases.csv')
 
 def exists_in_KB(phrase):
     global KB
@@ -106,6 +77,46 @@ def exists_in_KB(phrase):
        return 0 
        #import sys
        #print("Error: ",sys.exc_info()[1])  
+
+def optimal_phrases():
+    global bigram_vocab,trigram_vocab,unigram_vocab
+    optimal=[]
+    quality=[]
+    num_items=len(trigram_vocab.keys())
+    for key,v in trigram_vocab.items():
+        print("Original phrase:",key,"To go...",num_items)
+        if(v>0.25): 
+           optimal.append(key)
+           if(exists_in_KB(key)):
+             quality.append(1)
+           else:
+             quality.append(0)
+        num_items-=1
+
+    num_items=len(bigram_vocab.keys())
+    for key,v in bigram_vocab.items():
+        print("Original phrase:",key,"To go...",num_items)
+        if(v>0.25): 
+           optimal.append(key)
+           if(exists_in_KB(key)):
+             quality.append(1)
+           else:
+             quality.append(0)
+        num_items-=1
+
+    num_items=len(unigram_vocab.keys())
+    for key,v in unigram_vocab.items():
+        print("Original phrase:",key,"To go...",num_items)
+        if(v>0.25): 
+           optimal.append(key)
+           if(exists_in_KB(key)):
+             quality.append(1)
+           else:
+             quality.append(0)
+        num_items-=1
+
+    optimal_df=pd.DataFrame(dict(optimal_phrases=optimal,quality=quality)) 
+    optimal_df.to_csv('./Optimal_Phrases.csv',mode='a')
 
 def check_existence(d):
     print("**********************Checking existence******************")
@@ -125,22 +136,22 @@ def lower_keys(d):
 if __name__ =='__main__':
    KB=sort_dicts(KB)
    KB=lower_keys(KB)
-   print_dict(KB,"KB")
+   #print_dict(KB,"KB")
 
    bigram_vocab=rectify_frequencies(bigram_vocab)
    #bigram_vocab_pos=rectify_frequencies_Pos(bigram_vocab,bigram_pos)
    bigram_vocab=lower_keys(bigram_vocab)
    bigram_vocab=sort_dicts(bigram_vocab)
-   print_dict(bigram_vocab,"Bigrams")
-   check_existence(bigram_vocab)
+   #print_dict(bigram_vocab,"Bigrams")
+   #check_existence(bigram_vocab)
 
    trigram_vocab=rectify_frequencies(trigram_vocab)
    #trigram_vocab_pos=rectify_frequencies_Pos(trigram_vocab,trigram_pos)
    trigram_vocab=lower_keys(trigram_vocab)
    trigram_vocab=sort_dicts(trigram_vocab)
-   print_dict(trigram_vocab,"Trigrams")
-   check_existence(trigram_vocab)
-
+   #print_dict(trigram_vocab,"Trigrams")
+   #check_existence(trigram_vocab)
+   '''
    fourgram_vocab=rectify_frequencies(fourgram_vocab)
    fourgram_vocab=lower_keys(fourgram_vocab)
    fourgram_vocab=sort_dicts(fourgram_vocab)
@@ -158,12 +169,12 @@ if __name__ =='__main__':
    sixgram_vocab=sort_dicts(sixgram_vocab)
    print_dict(sixgram_vocab,"Sixgrams")
    check_existence(sixgram_vocab)
-
+   '''
    #should be done at the end - the other two depend on this.
    unigram_vocab=set_frequencies(unigram_vocab)
    unigram_vocab=lower_keys(unigram_vocab)
    unigram_vocab=sort_dicts(unigram_vocab)
-   print_dict(unigram_vocab,"Unigrams")
-   check_existence(unigram_vocab)
- 
-   #optimal_phrases()
+   #print_dict(unigram_vocab,"Unigrams")
+   #check_existence(unigram_vocab)
+    
+   optimal_phrases()
